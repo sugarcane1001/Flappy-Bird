e882b496519bdb443fa6bb3b7554c27fee3919c5
Zian <hasanzian6@gmail.com>
2024-05-27 17:21:20 -0400

    Added pictures

--- src/App.java
+++ src/App.java
@@ -4,16 +4,17 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         //Screen dimentions
-        int screenHeight = 360;
-        int screenWidth = 640;
-
+        int screenWidth = 360;
+        int screenHeight = 640;
+        
         JFrame jframe = new JFrame("Flappy Bird");
-        jframe.setSize(screenHeight, screenWidth);
+        jframe.setSize(screenWidth, screenHeight);
         jframe.setLocationRelativeTo(null);
         jframe.setResizable(false);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        FlappyBird flappyBird =  new FlappyBird(screenHeight, screenWidth);
+        //Instantiate and add the JPanel
+        FlappyBird flappyBird =  new FlappyBird(screenWidth, screenHeight);
         jframe.add(flappyBird);
         jframe.pack();
         jframe.setVisible(true);
--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,15 +1,44 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.awt.*;
-import java.util.*;
 import javax.swing.*;
 
 public class FlappyBird extends JPanel {
     
+    //Screen dimentions
     int screenHeight;
     int screenWidth;
 
+    //Images
+    Image backgroundImage;
+    Image birdImage;
+    Image topPipeImage;
+    Image bottomPipeImage;
+
     //Constructor
-    public FlappyBird(int screenHeight, int screenWidth){
-        setPreferredSize(new Dimension(screenHeight, screenWidth));
-        setBackground(Color.green);
+    public FlappyBird(int screenWidth, int screenHeight){
+        
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+
+        setPreferredSize(new Dimension(screenWidth, screenHeight));
+
+        //Load Images
+        backgroundImage = new ImageIcon(getClass().getResource("./images/flappybirdbg.png")).getImage();
+        birdImage = new ImageIcon(getClass().getResource("./images/flappybird.png")).getImage();
+        topPipeImage = new ImageIcon(getClass().getResource("./images/toppipe.png")).getImage();
+        bottomPipeImage = new ImageIcon(getClass().getResource("./images/bottompipe.png")).getImage();
+    }
+
+    public void paintComponent(Graphics g){
+        //invoke function from JPanel
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+
+        //background
+        g.drawImage(backgroundImage, 0, 0, screenWidth, screenHeight, null);
     }
 }
